d9c95384067d5ebf20e83c0d3c514e38839f8d68
farwasyed <farwa.asyed@gmail.com>
2023-03-12 02:15:53 +1100

    still unable to load thumbnail - wip

--- src/components/ListArticles.js
+++ src/components/ListArticles.js
@@ -1,7 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import capi from "../capi.json";
 
-export default function ListArticles() {
+function ListArticles() {
+  const [articles, setArticles] = useState(capi.results);
+  const [references, setReferences] = useState(capi.references);
+
+  useEffect(() => {
+    setReferences(capi.references);
+  }, []);
+
+  const getImageUrl = (id) => {
+    if (!id || !references[id] || !references[id].link || !references[id].link.media) {
+      return "";
+    }
+    return references[id].link.media.href;
+  };
+
   return (
-    <div>List of Articles Coming Soon!</div>
-  )
+    <div>
+      <h1>List of Articles</h1>
+      <ul>
+        {articles.map((article) => (
+          <li key={article.id}>
+            <h2>{article.title && article.title.title}</h2>
+            <p>{article.lifecycle && article.lifecycle.initialPublishDateTime}</p>
+            <p>{article.standFirst && article.standFirst.standfirst}</p>
+            <img
+              src={getImageUrl(article.related?.thumbnail?.default?.[0]?.id)}
+              alt="thumbnail"
+            />
+            <a href={article.canonicalWebLink && article.canonicalWebLink.url}>Read more</a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
+
+export default ListArticles;
